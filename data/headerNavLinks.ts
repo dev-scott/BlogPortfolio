3299c05865c63beff434be898b3dc24258c239bb
Dev-scott <sadoscott25@gmail.com>
2023-04-18 17:00:34 +0100

    update about.mdx

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   // { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  // { href: '/about', title: 'About' },
   { href: '/uses', title: 'Uses' },
 ]
 

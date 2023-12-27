0b8d0578427987413cea3ede3a1b9bd98b1670a5
Dev-scott <sadoscott25@gmail.com>
2023-12-27 11:50:44 +0100

    update favincon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,16 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/favicon.ico" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon.ico" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
+          />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
